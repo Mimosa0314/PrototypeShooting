9af3fc9627a76e9308c3dd188cad1fa6282140b2
Mimosa0314 <myfow14@gmail.com>
2026-07-11 17:06:20 +0900

    初回作成

---
+++ Assets/Scripts/PlayerMachine.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMachine : MonoBehaviour
+{
+    //敵の体力とかあるやつ
+    //[SerializeField] private Enemy enemy; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //攻撃ボタン
+        if (Input.GetKeyDown(KeyCode.Space)){
+            //敵の判定受付メソッド呼び出し
+            /*
+             * enemy.TakeDamage();
+             * 
+             */
+        }
+        
+    }
+}
