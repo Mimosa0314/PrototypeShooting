047488ee73d074efe0cac1916eddf4651d4e5c62
Mimosa0314 <myfow14@gmail.com>
2026-07-18 23:17:55 +0900

    素材アセット追加

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -2,6 +2,14 @@ using   UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
+
+    [SerializeField] private GameObject bullet;
+    private float bulletSpeed = 5f;
+
+    public PlayerBullet()
+    {
+
+    }
     void Start()
     {
         
@@ -9,12 +17,16 @@ public class PlayerBullet : MonoBehaviour
 
     void Update()
     {
-        
+        /*’e‚Ì‹““®
+         * í‚Éˆê’è•ûŒü‚Éˆ—‚·‚é
+         */
+        Vector2 bulletPos = transform.position;
+
     }
 
     //’e‚Ì”­Ë
     public void Firing(Vector2 pos)
     {
-
+        Instantiate(bullet, pos, Quaternion.identity);
     }
 }
