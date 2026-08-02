1d8b791fb4db0ead4a74a127fea410fe0ef81bce
小津 <gamesend2025@gmail.com>
2026-08-02 20:49:25 +0900

    変更

---
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerMachine : MonoBehaviour
+{
+    private int life = 1;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        /*
+         *“G’e‚Æ‚ÌÚG”»’è 
+         */
+        if (collision.gameObject.tag == "enemyBullet")
+        {
+            Death();
+        }
+    }
+
+    /*
+     * €–S
+     */
+    void Death()
+    {
+        //‘Ì—Í‚ğ0‚É‚·‚é
+        life = 0;
+        Debug.Log("“G’e‚ÆÚGA€–S‚µ‚Ü‚µ‚½");
+    }
+    public void TakeDamage()
+    {
+        Debug.Log("ƒvƒŒƒCƒ„[‚ªUŒ‚‚ğó‚¯‚Ü‚µ‚½");@
+    }
+}
