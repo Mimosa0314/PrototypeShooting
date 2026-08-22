98d4fdfdcbc681bbdcec7e0b8b095b516c8dbf21
Mimosa0314 <myfow14@gmail.com>
2026-08-22 22:26:03 +0900

    機体が画面外に出るのを防止（上側）

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -34,46 +34,73 @@ public class PlayerController : MonoBehaviour
         Vector3 buttonLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
         Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
 
-        Debug.Log(playerCollider.bounds.max);
-        Debug.Log(topRight);
 
-        if (playerCollider.bounds.max.y == topRight.y)
+        if (playerCollider.bounds.max.y >= topRight.y && y > 0)
         {
-            /*
-             * 画面上端では上入力できないようにする
-             */
-            Vector3 move = new Vector3(x, 0, 0);
-            transform.position += move * moveSpeed * Time.deltaTime;
 
-            Debug.Log("上端");
+            if(playerCollider.bounds.min.x <= buttonLeft.x && x < 0)
+            {
+                /*
+                 * 左上かつ左入力の場合移動させない
+                 */
+
+            }else if(playerCollider.bounds.min.x <= buttonLeft.x && x > 0)
+            {
+                /*
+                 * 左上かつ右入力の場合
+                 */
+                Vector3 move = new Vector3(x, 0, 0);
+                transform.position += move * moveSpeed * Time.deltaTime;
+
+            }else if (playerCollider.bounds.min.x >= topRight.x && x > 0)
+            {
+                /*
+                 * 右上かつ右入力の場合移動させない
+                 */
+
+            }
+            else if (playerCollider.bounds.min.x >= topRight.x && x > 0)
+            {
+                /*
+                 * 右上かつ左入力の場合
+                 */
+                Vector3 move = new Vector3(x, 0, 0);
+                transform.position += move * moveSpeed * Time.deltaTime;
+
+            } else
+            {
+                /*
+                 * 画面上端では上入力できないようにする
+                 */
+                Vector3 move = new Vector3(x, 0, 0);
+                transform.position += move * moveSpeed * Time.deltaTime;
+            }
+
 
         }
-        else if (playerCollider.bounds.min.y == buttonLeft.y && y < 0)
+        else if (playerCollider.bounds.min.y <= buttonLeft.y && y < 0 )
         {
             /*
              * 画面下端では下入力できないようにする
              */
             Vector3 move = new Vector3(x, 0, 0);
             transform.position += move * moveSpeed * Time.deltaTime;
-            Debug.Log("下端");
         }
-        else if (playerCollider.bounds.min.x == buttonLeft.x && x < 0)
+        else if (playerCollider.bounds.min.x <= buttonLeft.x && x < 0)
         {
             /*
              * 画面左端では左入力できないようにする
              */
             Vector3 move = new Vector3(0, y, 0);
             transform.position += move * moveSpeed * Time.deltaTime;
-            Debug.Log("左端");
         }
-        else if (playerCollider.bounds.max.x == topRight.x && x > 0)
+        else if (playerCollider.bounds.max.x >= topRight.x && x > 0)
         {
             /*
              * 画面右端では右入力できないようにする
              */
             Vector3 move = new Vector3(0, y, 0);
             transform.position += move * moveSpeed * Time.deltaTime;
-            Debug.Log("右端");
 
         }
         else
