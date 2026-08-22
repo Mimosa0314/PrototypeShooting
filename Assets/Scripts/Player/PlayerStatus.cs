7c057141b28aef4f5bed1ae39a95f0997f9d4ea1
Mimosa0314 <myfow14@gmail.com>
2026-08-22 11:36:35 +0900

    強豪解消とtestscene削除

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -1,10 +1,6 @@
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/Player/PlayerStatus.cs
 public class PlayerStatus: MonoBehaviour
-========
-public class PlayerStatus : MonoBehaviour
->>>>>>>> 41393cb1c7e1d4994c937c779f3edb4114ad571a:Assets/Scripts/PlayerManager/PlayerStatus.cs
 {
     private int life = 1;
    
