a5ec6d00e7c78e86fa6f8e4fafb5d3087d1b0465
Mimosa0314 <myfow14@gmail.com>
2026-09-17 22:45:12 +0900

    敵機の攻撃用スクリプト追加

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        //“G‹@‚Æ’¼ÚÚG‚µ‚½ê‡‚Éƒ_ƒ[ƒW‚ğ—^‚¦‚é
+        if(collision.gameObject.tag == "player")
+        {
+            PlayerStatus playerStatus = player.GetComponent<PlayerStatus>();
+        }
+    }
+}
